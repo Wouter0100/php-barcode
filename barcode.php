f198f0c2ed2c5b46a5989619f50857684e4489bc
Mauro Parolari <mauro@bjmaster.com>
2014-01-30 18:30:46 +0100

    Removed class call (put it in separate example file)

--- barcode.php
+++ barcode.php
@@ -30,9 +30,3 @@ class Barcode
      return substr(number_format(time() * rand(),0,'',''),0,12);
    }
 }
-
-$encoding = (isset($_GET['encoding'])) ? $_GET['encoding'] : 'EAN-13';
-$number   = (isset($_GET['code']))     ? $_GET['code']     : null;
-$scale    = (isset($_GET['scale']))    ? $_GET['scale']    : null;
-
-new Barcode($encoding, $number, $scale);
